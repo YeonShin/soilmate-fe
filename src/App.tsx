7246163e99a42d2d3b1e10a9ac87a1468859d13f
YeonShin <yeonnim82@gmail.com>
2025-06-05 07:13:38 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+    return (
+        <>
+            <h1 className="col-end-1 text-3xl font-bold">Hello world!</h1>
+            <button className="text-2xl font-bold">2</button>
+        </>
+    );
+};
+
+export default App;
