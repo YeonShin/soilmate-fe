a661a4ce92238fa960bf09c30bd2ae889f851da5
YeonShin <yeonnim82@gmail.com>
2025-06-12 01:55:07 +0900

    fix: 프록시 설정 변경 (#9)

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,17 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+    server: {
+    port: 5173,
+    proxy: {
+      // 클라이언트에서 /api로 시작하는 요청은 localhost:8080으로 전달
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+        // (선택) /api → /api 매핑을 그대로 쓸 거면 rewrite 생략 가능
+        rewrite: (path) => path,
+      },
+    },
+  },
 })
