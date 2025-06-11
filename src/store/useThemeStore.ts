7200241529f43826e4252073d6659aeb7a744ace
YeonShin <yeonnim82@gmail.com>
2025-06-12 00:55:26 +0900

    feat: 다크모드 전역 상태 로컬스토리지 저장

--- src/store/useThemeStore.ts
+++ src/store/useThemeStore.ts
@@ -1,12 +1,23 @@
 // src/store/useThemeStore.ts
 import { create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
 
 interface ThemeState {
-  dark: boolean;
-  toggle: () => void;
+  dark: boolean
+  toggle: () => void
+  setDark: (val: boolean) => void
 }
 
-export const useThemeStore = create<ThemeState>()((set) => ({
-  dark: false,
-  toggle: () => set((state) => ({ dark: !state.dark })),
-}));
+export const useThemeStore = create<ThemeState>()(
+  persist(
+    (set, get) => ({
+      dark: false,
+      toggle: () => set({ dark: !get().dark }),
+      setDark: (val) => set({ dark: val }),
+    }),
+    {
+      name: 'soilmate-theme',                          // localStorage 키
+      storage: createJSONStorage(() => localStorage), // 여기!
+    }
+  )
+)
